2297e870ef5978cfc3b3a47be5e2da0c0d7d51db
muzammil-85 <muzukondeth85@gamil.com>
2022-06-20 21:51:57 +0530

    commented statements

--- base/admin.py
+++ base/admin.py
@@ -2,4 +2,6 @@ from atexit import register
 from django.contrib import admin
 from models import *
 # Register your models here.
-admin.site.register(modelsclass)
+
+
+# admin.site.register(modelsclass)
--- base/models.py
+++ base/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class modelsclass(models.Model):
-    user = models.OneToOneField(
-        User, null=True, blank=True, on_delete=models.CASCADE)
-    pass
+
+# class modelsclass(models.Model):
+#     user = models.OneToOneField(
+#         User, null=True, blank=True, on_delete=models.CASCADE)
+#     pass
